c4f6d5f389c1a5409d48d9859599399d80b98cd2
Maarit <p0033@student.jamk.fi>
2020-11-10 14:12:44 +0200

    fixing tags

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -91,7 +91,7 @@ class Editor extends React.Component {
         fieldWords = this.props.title.split(" ")
       }
       else if(curseField === "body") fieldWords = this.props.body.split(" ")
-      else if(curseField === "tagit") fieldWords = this.props.tagList.split(" ")
+      else if(curseField === "tagit") fieldWords = this.props.tagList.toArray()
       else if(curseField === "description") fieldWords = this.props.description.split(" ")
       for (let i= 0; i < fieldWords.length; i++){
         if(badWords.indexOf(fieldWords[i]) > -1){
