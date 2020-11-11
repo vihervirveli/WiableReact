b9f3613964b89c21566b09173764d069a253fb50
Maarit <p0033@student.jamk.fi>
2020-11-11 13:55:50 +0200

    async/await

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -44,10 +44,13 @@ class Editor extends React.Component {
     this.watchForSpace = ev => {
       if (ev.keyCode === 32) { //enter === 13
         ev.preventDefault();
-        this.props.onAddTag();
-        this.testingForBadWords(
-          "#tagit","#tagitContainer", 
-          "tagit")
+        async function(){
+          this.props.onAddTag();
+        await this.testingForBadWords(
+            "#tagit","#tagitContainer", 
+            "tagit")
+  
+        }
       }
     };
 
