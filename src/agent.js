4b6a02d8fc9b3a284b6938544241d483772b43fe
Tatiane Forster <AA5679@student.jamk.fi>
2020-11-19 14:51:39 +0200

    gitignore doesn't ignore again.. rollback for agent.js

--- src/agent.js
+++ src/agent.js
@@ -5,7 +5,7 @@ const superagent = superagentPromise(_superagent, global.Promise);
 
 //const API_ROOT = 'https://conduit.productionready.io/api';
 //const API_ROOT = 'http://localhost:3000/api';
-//const API_ROOT = process.env.REACT_APP_API_ROOT || 'https://conduit.productionready.io/api';
+const API_ROOT = process.env.REACT_APP_API_ROOT || 'https://conduit.productionready.io/api';
 //const API_ROOT = process.env.REACT_APP_API_ROOT || 'http://localhost:3000/api';
 // .
 
